5bcb79dbe83c84fecd7edd2e7daaeacb5f8b35d8
coporteer <ohsungwoo@dankook.ac.kr>
2023-11-14 21:36:44 +0900

    simulink_added

--- ICE2.m
+++ ICE2.m
@@ -3,13 +3,17 @@ clc
 
 % This code is for matlab simulation which is about Lyapunov`s stability theorm
 
+% simulink related
+% https://kr.mathworks.com/help/simulink/ug/what-is-a-matlab-function-block.html
+% https://kr.mathworks.com/help/simulink/slref/add.html
+
 % https://kr.mathworks.com/help/matlab/matlab_prog/comments.html
 % Possibly error range
 eps = 1e-6;
 
 % Array list
 A = [0 1;
-    1 2];
+    0 2];
 
 B = [0; 1];
 
@@ -20,9 +24,10 @@ m = size(B,2);
 % https://yalmip.github.io/command/sdpvar/
 X = sdpvar(n,n);
 M = sdpvar(m,n);
+Al = 100;
 
 lmi1 = X - eps*eye(n) >= 0;
-lmi2 = A*X + X*A' + B*M + M'*B' + eps*eye(n) <= 0;
+lmi2 = 2*Al*A + A*X + X*A' + B*M + M'*B' + eps*eye(n) <= 0;
 
 lmis = [lmi1; lmi2];
 
